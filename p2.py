d950360fa390823465b593afc282b3ec9f730a04
HARSH JANGHEL <harshjanghel875@gmail.com>
2025-03-06 20:12:58 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,7 @@
+p=float(input("enter principal amount:"))
+r=float(input("enter rate of interest:"))
+t=float(input("enter time:"))
+SI=(p*r*t)/100
+CI=(p*r*t)
+print("Simple Interest:",SI)
+print("Cumulative Interest:",CI)
