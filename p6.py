11166b3b6de6ae24d35a480984d4642ae8944e23
HARSH JANGHEL <harshjanghel875@gmail.com>
2025-03-15 00:45:35 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,13 @@
+b=float(input("enter breadth:"))
+h=float(input("enter height:"))
+l=float(input("enter length:"))
+r=float(input("enter breadth:"))
+tri=(b*h)/2
+rect=l*b
+crc=3.14*r*r
+sph=4*3.14*r*r
+print("area of triangle:",tri)
+print("area of rectangle:",rect)
+print("area of circle:",crc)
+print("area of sphere:",sph)
+
