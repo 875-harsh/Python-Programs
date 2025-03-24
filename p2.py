41a80db4afaf9ae153fbe6ae90b4081045f1d302
HARSH JANGHEL <harshjanghel875@gmail.com>
2025-03-24 10:02:38 +0530

    Add files via upload

--- p2.py
+++ p2.py
@@ -1,7 +1,13 @@
-p=float(input("enter principal amount:"))
-r=float(input("enter rate of interest:"))
-t=float(input("enter time:"))
-SI=(p*r*t)/100
-CI=(p*r*t)
-print("Simple Interest:",SI)
-print("Cumulative Interest:",CI)
+# Input: Name from the user
+name = input("Enter your name: ")
+
+# Reverse the name
+reversed_name = name[::-1]
+
+# Get the last 5 characters of the reversed name
+last_five_characters = reversed_name[:5]
+
+# Output the result
+print(f"The reversed name is: {reversed_name}")
+print(f"The last 5 characters of the reversed name are: {last_five_characters}")
+
