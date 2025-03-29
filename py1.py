5647deff863402e75560c95d8c02b9f8254e4fe4
HARSH JANGHEL <harshjanghel875@gmail.com>
2025-03-30 04:13:51 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,18 @@
+def area(l,w):
+    print("area:",l*w)
+    return l,w
+def main():
+    l=int(input("enter length::"))
+    w=int(input("enter width::"))
+    x=area(l,w)
+    print(x)
+if __name__=="__main__":
+    main()
+
+
+
+
+
+
+
+    
